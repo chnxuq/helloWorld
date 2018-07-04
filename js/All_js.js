dfe44e05acf55b866eaf3ee34cc817c72bfa791b
chnxuq <35512589+chnxuq@users.noreply.github.com>
2018-07-04 14:07:39 +0800

    add helloWorld

---
+++ js/All_js.js
@@ -0,0 +1,110 @@
+﻿ 
+//Cookie
+$.Cookie = {
+	add: function (name, value) { $.cookie(name, value); },
+	add: function (name, value, day) { $.cookie(name, value, { expires: day }); },
+	add: function (name, value, day, path) { $.cookie(name, value, { expires: day, path: path }); },
+	set: function (name) { $.cookie(name); },
+	move: function (name) { $.cookie(name, null); }
+}
+//SubString  截取字符串
+$.SubString = {
+    //盒子 - 内容 - 长度 - 省略符
+    c: function (box_, find_, length_, string_) {$(box_).each(function () {if ($(this).find(find_).text().length > length_) {var b = $(this).find(find_);var c = b.text().substring(0, length_);b.html(c + string_)}})},
+	//盒子 - 长度 - 省略符
+	w: function (box_, length_, string_) {$(box_).each(function () {if ($(this).text().length > length_) {var c = $(this).text().substring(0, length_);$(this).text(c + string_);}})},
+    //盒子 - 长度
+    s: function (box_, length_) {$(box_).each(function () {if ($(this).text().length > length_) {var c = $(this).text().substring(0, length_);$(this).text(c);}})} 
+} 
+//msg_box   弹出悬浮窗  居中
+$.Box_fixed = {
+    //box—弹出的盒子
+    //open_check—点击打开事件触发
+    //close_check—关闭事件触发
+	z: function (box, open_check, close_check) {
+
+		$(open_check).click(function () { 
+            var box_height = $(box).height();
+            var box_width = $(box).width();
+            var win_height = $(window).height();
+			var win_width = $(window).width();
+		 
+            $(box).css("position", "fixed").css("top", (win_height - box_height) / 2).css("left", (win_width - box_width) / 2);
+			$(box).show();
+			$(window).resize(function () {
+				$(box).css("position", "fixed").css("top", (win_height - box_height) / 2).css("left", (win_width - box_width) / 2);
+			});
+        })
+        $(close_check).click(function () {
+            $(box).hide();
+        })
+    },
+    //box—弹出的盒子
+    //open_check—点击打开事件触发
+    //close_check—关闭事件触发
+    //mb—灰色背景层触发
+    m: function (box, open_check, close_check, mb) {
+        $(open_check).click(function () {
+            var box_height = $(box).height();
+            var box_width = $(box).width();
+            var win_height = $(window).height(); 
+            var win_width = $(window).width();
+            $(box).css("position", "fixed").css("top", (win_height - box_height) / 2).css("left", (win_width - box_width) / 2);
+            $(box).show(); $(mb).show();// $("body").css("overflow", "hidden"); 
+        })
+        $(close_check).click(function () {
+            $(box).hide(); $(mb).hide(); //$("body").css("overflow", "auto");
+        })
+        $(mb).click(function () {
+            $(box).hide(); $(mb).hide();// $("body").css("overflow", "auto");
+        })
+    }
+}  
+//点击box   返回到顶部需要time秒
+$.return_top = {a: function (box, time) {$(box).click(function () {var speed = time;$('body,html').animate({ scrollTop: 0 }, speed);return false;});}}
+//关闭浏览器触发的事件
+$.close_form = { close: function (close_Message,reutrn_Message) { window.onbeforeunload = function () {if (document.all) {if (event.clientY < 0) {return reutrn_Message;}} else {return close_Message;}}}}
+//执行函数   
+$.delay_function = {
+    //暂停time秒执行funcions方法  只执行一次 
+    once: function (functions,time) {setTimeout(functions, time);},
+    //一直执行functions方法  time秒执行一次
+    loop: function (functions, time) {setInterval(functions, time);},
+    //停止所有函数运行
+    clear:function(){ clearInterval()}
+} 
+//加入收藏 
+function AddFavorite(sURL, sTitle) {
+    sURL = encodeURI(sURL);
+    try {window.external.addFavorite(sURL, sTitle);
+	} catch (e) {
+		try {window.sidebar.addPanel(sTitle, sURL, "");
+        } catch (e) {alert("加入收藏失败，请使用Ctrl+D进行添加,或手动在浏览器里进行设置.");}
+    }
+}
+
+//时间   division--分隔符
+$.time = {
+	years: function () { var d = new Date(); var vYear = d.getFullYear(); return vYear; },
+	month: function () { var d = new Date(); var vMon = d.getMonth() + 1; s = vMon < 10 ? "0" + vMon : vMon; return s; },
+	day: function () { var d = new Date(); var vDay = d.getDate(); s = vDay < 10 ? "0" + vDay : vDayreturns; },
+	hour: function () { var d = new Date(); var h = d.getHours(); s = h < 10 ? "0" + h : h; return s; },
+	minute: function () { var d = new Date(); var m = d.getMinutes(); s = m < 10 ? "0" + m : m; return s; },
+	second: function () { var d = new Date(); var se = d.getSeconds(); s = se < 10 ? "0" + se : se; return s; },
+	yyyy_mm_dd: function (division) { var d = new Date(); var vYear = (d.getFullYear()); var vMon = d.getMonth() + 1; var vDay = d.getDate(); s = vYear + division + (vMon < 10 ? "0" + vMon : vMon) + division + (vDay < 10 ? "0" + vDay : vDay); return s; },
+	hh_mm: function (division) { var d = new Date(); var h = d.getHours(); var m = d.getMinutes(); s = (h < 10 ? "0" + h : h) + division + (m < 10 ? "0" + m : m); return s; },
+	hh_mm_ss: function (division) { var d = new Date(); var h = d.getHours(); var m = d.getMinutes(); var se = d.getSeconds(); s = (h < 10 ? "0" + h : h) + division + (m < 10 ? "0" + m : m) + division + (se < 10 ? "0" + se : se); return s; }
+}; 
+
+//// 传参数 
+//var aa = function (x) {
+//    //弹出对象 x 里的 aa 变量和 bb 变量 
+//    alert(x.aa + " 我成功啦 " + x.bb);
+//}
+
+//$.windowbox = aa;
+
+//$.windowbox({
+//    aa: "哈哈",
+//    bb: "啦啦"
+//});
---
+++ js/jquery.cookie.js
@@ -0,0 +1,148 @@
+/**
+ * Create a cookie with the given name and value and other optional parameters.
+ *
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Set the value of a cookie.
+ * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
+ * @desc Create a cookie with all available options.
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Create a session cookie.
+ * @example $.cookie('the_cookie', null);
+ * @desc Delete a cookie by passing null as value. Keep in mind that you have to use the same path and domain
+ *       used when the cookie was set.
+ *
+ * @param String name The name of the cookie.
+ * @param String value The value of the cookie.
+ * @param Object options An object literal containing key/value pairs to provide optional cookie attributes.
+ * @option Number|Date expires Either an integer specifying the expiration date from now on in days or a Date object.
+ *                             If a negative value is specified (e.g. a date in the past), the cookie will be deleted.
+ *                             If set to null or omitted, the cookie will be a session cookie and will not be retained
+ *                             when the the browser exits.
+ * @option String path The value of the path atribute of the cookie (default: path of page that created the cookie).
+ * @option String domain The value of the domain attribute of the cookie (default: domain of page that created the cookie).
+ * @option Boolean secure If true, the secure attribute of the cookie will be set and the cookie transmission will
+ *                        require a secure protocol (like HTTPS).
+ * @type undefined
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+
+/**
+ * Get the value of a cookie with the given name.
+ *
+ * @example $.cookie('the_cookie');
+ * @desc Get the value of a cookie.
+ *
+ * @param String name The name of the cookie.
+ * @return The value of the cookie.
+ * @type String
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+
+
+
+jQuery.cookie = function(name, value, options) {
+    if (typeof value != 'undefined') { // name and value given, set cookie
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options = $.extend({}, options); // clone object since it's unexpected behavior if the expired property were changed
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+        }
+        // NOTE Needed to parenthesize options.path and options.domain
+        // in the following expressions, otherwise they evaluate to undefined
+        // in the packed version for some reason...
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else { // only name given, get cookie
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
+
+
+            function cookie(name) {
+                var cookieArray = document.cookie.split("; "); //得到分割的cookie名值对    
+                var cookie = new Object();
+                for (var i = 0; i < cookieArray.length; i++) {
+                    var arr = cookieArray[i].split("=");       //将名和值分开    
+                    if (arr[0] == name) return unescape(arr[1]); //如果是指定的cookie，则返回它的值    
+                }
+                return "";
+            }
+ 
+            //删除cookie
+            function delCookie(name) {
+                document.cookie = name + "=;expires=" + (new Date(0)).toGMTString();
+            }
+
+            //获取指定名称的cookie的值
+            function getCookie(objName) {
+                var arrStr = document.cookie.split("; ");
+                for (var i = 0; i < arrStr.length; i++) {
+                    var temp = arrStr[i].split("=");
+                    if (temp[0] == objName) return unescape(temp[1]);
+                }
+            } 
+            //添加cookie
+            function addCookie(objName, objValue, objHours) {  
+                var str = objName + "=" + escape(objValue);
+                if (objHours > 0) {     //为时不设定过期时间，浏览器关闭时cookie自动消失
+                    var date = new Date();
+                    var ms = objHours * 3600 * 1000;
+                    date.setTime(date.getTime() + ms);
+                    str += "; expires=" + date.toGMTString();
+                }
+                document.cookie = str;
+            } 
+            //两个参数，一个是cookie的名子，一个是值
+            function SetCookie(name, value) {    
+                var Days = 30;  //此 cookie 将被保存 30 天
+                var exp = new Date();    //new Date("December 31, 9998");
+                exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+                document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+            } 
+            //取cookies函数
+            function getCookie(name) {      
+                var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+                if (arr != null) return unescape(arr[2]); return null;
+            }
+
+            //删除cookie
+            function delCookie(name) {  
+                var exp = new Date();
+                exp.setTime(exp.getTime() - 1);
+                var cval = getCookie(name);
+                if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+            }
+
+            
